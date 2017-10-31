d90254b37b7d12ecbfa239a3c9a1a9c9c1085e7e
Gaurav Rawal <gaurav.rawal66@gmail.com>
2017-10-31 19:01:21 +0530

    Transitions Added

--- js/script.js
+++ js/script.js
@@ -2,18 +2,76 @@ $(document).ready(function () {
     //Init the controller
     var controller = new ScrollMagic.Controller();
 
+    //Init wow animation object
+    new WOW().init();
+
     //pin the pixorr logo
     new ScrollMagic.Scene({
         triggerElement: '#features-container',
         duration: '300%',
-        triggerHook: 0.1
+        triggerHook: 0.7
     })
         .setPin('#pixorr-logo',{ pushFollowers:false})
         .addTo(controller)
-        .setTween(TweenMax.to("#pixorr-logo-image", 1, {borderRadius: '50%' } ));
+        .setTween(TweenMax.to("#pixorr-logo-image", 1, {borderRadius: '55%' } ));
 
-    //
-    var Transitions = new ScrollMagic.Scene({
+    //Transition 1 image
+    new ScrollMagic.Scene({
+      triggerElement:'#feature-1',
+      triggerHook: 1,
+      duration: '110%',
+      reverse: false
+    })
+        .setClassToggle('#feature-1-image', 'slideInLeft')
+        .addTo(controller);
+    
+    //Transition 1 text
+    new ScrollMagic.Scene({
+      triggerElement:'#feature-1',
+      triggerHook: 1,
+      duration: '110%',
+      reverse: false
+    })
+        .setClassToggle('#feature-1-text', 'fadeInUp')
+        .addTo(controller);
+    
+    //Transition 2 image
+    new ScrollMagic.Scene({
+      triggerElement:'#feature-2',
+      triggerHook: 1,
+      duration: '110%',
+      reverse: false
+    })
+        .setClassToggle('#feature-2-image', 'slideInRight')
+        .addTo(controller);
+    
+   //Transition 2 text
+    new ScrollMagic.Scene({
+      triggerElement:'#feature-2',
+      triggerHook: 1,
+      duration: '110%',
+      reverse: false
+    })
+        .setClassToggle('#feature-2-text', 'fadeInUp')
+        .addTo(controller);
 
-    });
+    //Transition 3 image
+    new ScrollMagic.Scene({
+      triggerElement:'#feature-3',
+      triggerHook: 1,
+      duration: '110%',
+      reverse: false
+    })
+        .setClassToggle('#feature-3-image', 'slideInLeft')
+        .addTo(controller);
+    
+   //Transition 3 text
+    new ScrollMagic.Scene({
+      triggerElement:'#feature-3',
+      triggerHook: 1,
+      duration: '110%',
+      reverse: false
+    })
+        .setClassToggle('#feature-3-text', 'fadeInUp')
+        .addTo(controller);
 });
